6467c60bfd59c5049fd02bb453f6a2fb56407424
xjingx <46096090+xjingx@users.noreply.github.com>
2021-02-25 17:37:00 +0800

    安装依赖包

--- vue3-component-project/src/components/HelloWorld.vue
+++ vue3-component-project/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}{{ age }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -103,13 +103,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+    age: Number,
+  },
 });
 </script>
 
